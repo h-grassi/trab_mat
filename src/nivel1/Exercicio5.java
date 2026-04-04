ced8f373c24a2057770b04ccc55d890328275a86
Henrique C Grassi <107431970+h-grassi@users.noreply.github.com>
2026-04-04 18:30:10 +0000

    exerc 5 e 7

--- src/nivel1/Exercicio5.java
+++ src/nivel1/Exercicio5.java
@@ -7,12 +7,13 @@ public class Exercicio5 {
     public void executar() {
         System.out.println("Exercício 5: Remover duplicatas (sem ordenar)");
         System.out.println("Em construção");
-        /*
+        
         Random aleatorio = new Random();
         int val_aleat_min = 1;
         int val_aleat_max = 5;
         int [] vetor = new int[10];
-
+        int [] vet_resultado = new int[10];
+        int indice = 0;
         System.out.print("Vetor original: [");
         for(int i = 0; i<10 ; i ++){
             vetor[i] = aleatorio.nextInt((val_aleat_max - val_aleat_min) + 1) + val_aleat_min;
@@ -23,12 +24,28 @@ public class Exercicio5 {
         }
         System.out.println("]\n");
 
+        //cria vetor resultado
         for(int i=0; i <= vetor.length - 1; i++){
-            int [] aux_vet = new int[vetor.length];
-            if(vetor[i]== aux_vet[i])
+            int insere_num = 1; //se zero não insere, se um insere
             
+            for(int x = 0; x< vetor.length; x++){
+                if(vetor[i] == vet_resultado[x]){
+                    insere_num =0;
+                }
+            }
+            if(insere_num == 1){
+                vet_resultado[indice] = vetor[i];
+                indice++;
+            }
         } 
-        System.out.println(""); */
-}}
-
-// System.out.println("aux: "+ aux_vet[x]); //System.out.println("Valor de i: " + vetor[i] + " | Valor de x: " + vetor[x]);
+        //exibe vetor resultado
+        System.out.print("Vetor resultado: [");
+        for(int i = 0; i<5 ; i ++){
+            System.out.print(vet_resultado[i]);
+            if(i< vet_resultado.length -1 && vet_resultado[i+1] !=0){
+               System.out.print( ",");
+            } 
+        }
+        System.out.println("]\n");
+    }   
+}
